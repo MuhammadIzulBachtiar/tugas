8d714ab08491d51bde437ac1e74fe31bd5dc33dd
Muhammad Izul Bachtiar 1900016126 <izulbahtiar1@gmail.com>
2021-04-20 12:14:40 +0700

    revisi vidio 4

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialDesign } from '../material/material';
+import { ImagesComponent } from './images/images.component';
 
 const routes: Routes = [
   {
@@ -14,13 +15,16 @@ const routes: Routes = [
         path:'dashboard',
         component:DashboardComponent
       },
-      
+      {
+        path:'images',
+        component:ImagesComponent
+      }
     ]
   }
 ]
 
 @NgModule({
-  declarations: [AdminComponent, DashboardComponent],
+  declarations: [AdminComponent, DashboardComponent, ImagesComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -12,4 +12,23 @@ export class AdminComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  menu=[
+    {
+      name:'Dashboard',
+      icon:'dashboard',
+      url:'/admin/dashboard'
+    },
+    {
+      group:'Menu Group',
+      children:[
+        {
+          name:'Image Gallery',
+          icon:'images',
+          url:'/admin/images'
+        }
+      ]
+    }
+  ];
+
+
 }
