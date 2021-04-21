fbda9838c74bbebca2805d4c2356af44fd705f6d
Muhammad Izul Bachtiar 1900016126 <izulbahtiar1@gmail.com>
2021-04-21 12:42:36 +0700

    vidio 5

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialDesign } from '../material/material';
 import { ImagesComponent } from './images/images.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -18,17 +21,28 @@ const routes: Routes = [
       {
         path:'images',
         component:ImagesComponent
+      },
+      {
+        path:'product',
+        component:ProductComponent
+      }, 
+      {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'/admin/dashboard'
       }
+ 
     ]
   }
 ]
 
 @NgModule({
-  declarations: [AdminComponent, DashboardComponent, ImagesComponent],
+  declarations: [AdminComponent, DashboardComponent, ImagesComponent, ProductComponent, ProductDetailComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MaterialDesign
+    MaterialDesign,
+    FormsModule
   ]
 })
 export class AdminModule { }
