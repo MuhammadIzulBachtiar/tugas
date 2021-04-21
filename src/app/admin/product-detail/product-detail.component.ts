074bbdaa6aa7d2e53e20b0f866a75ec70b57802c
Muhammad Izul Bachtiar 1900016126 <izulbahtiar1@gmail.com>
2021-04-21 20:12:58 +0700

    vidio 6

--- src/app/admin/product-detail/product-detail.component.ts
+++ src/app/admin/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
  selector: 'app-product-detail',
@@ -9,15 +10,37 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class ProductDetailComponent implements OnInit {
  constructor(
    public dialogRef:MatDialogRef<ProductDetailComponent>,
-   @Inject(MAT_DIALOG_DATA) public data: any
+   @Inject(MAT_DIALOG_DATA) public data: any,
+   public api:ApiService
  )
  {
  
  }
  ngOnInit(): void {
  }
+ loading:boolean | undefined;
  saveData()
  {
-   this.dialogRef.close(this.data);
- }
+  this.loading=true;
+   if(this.data.id == undefined)
+   {
+     this.api.post('books',this.data).subscribe(result=>{
+       this.dialogRef.close(result);
+       this.loading=false;
+     },error=>{
+      this.loading=false;
+       alert('tidak dapat menyimpan data');
+       
+     })
+   }else{     
+    this.api.put('books/'+this.data.id,this.data).subscribe(result=>{
+       this.dialogRef.close(result);
+       this.loading=false;
+     },error=>{
+      this.loading=false;
+       alert('tidak dapat memperbarui data');
+       
+     })
+   }  
+  }
 }
--- src/app/admin/product/product.component.ts
+++ src/app/admin/product/product.component.ts
@@ -1,80 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
-
+import { ApiService } from 'src/app/services/api.service';
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+ selector: 'app-product',
+ templateUrl: './product.component.html',
+ styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-  title:any;
-  book:any={};
-  books:any=[];
-  constructor(
-    public dialog:MatDialog
-  ) {
+ title:any;
+ book:any={};
+ books:any=[];
+  id: string | undefined;
+ constructor(
+   public dialog:MatDialog,
+   public api:ApiService
+ )
+ {
+   this.title='Produk'; 
+   this.getBooks();   
+ }
+ ngOnInit(): void {
+  
+ }
+ loading:boolean | undefined;
+ getBooks()
+ {
+  this.loading=true;
+   this.api.get('books').subscribe(result=>{
+     this.books=result;
+     this.loading=false;
+   }, _error=>{
+    this.loading=false;
+    alert('Tidak dapat mengambil data');
+  })     
+ }
+ productDetail(data: any,idx: number)
+ {
+   let dialog=this.dialog.open(ProductDetailComponent, {
+     width:'400px',
+     data:data
+   });
+   dialog.afterClosed().subscribe(res=>{
+     if(res)
+     {
+       if(idx==-1)this.books.push(res);
+       else this.books[idx]=data;
+     }
+   })
+ }
+ loadingDelete:any={};
+ deleteProduct(idx: string | number)
+ {
+   var conf=confirm('Delete item?');
+   if(conf)
+   {
+    this.loadingDelete[idx]=true;
+     this.api.delete('books/'+this.books[idx].id).subscribe(_result=>{
+       this.books.splice(idx,1);
+       this.loadingDelete[idx]=false;
+     },_error=>{
+      this.loadingDelete[idx]=false;
+       alert('Tidak dapat menghapus data');
+     });
+    }
   }
-
-  ngOnInit(): void {
-    this.title='Products';
-    this.book={
-      title:'Angular untuk Pemula',
-      author:'Farid Suryanto',
-      publisher:'Sunhouse Digital',
-      year:2020,
-      isbn:'8298377474',
-      price:70000
-    };
-    this.getBooks();
-  }
-
-  getBooks()
-  {
-    //4. memperbarui koleksi books
-    this.books=[
-      {
-        title:'Angular untuk Pemula',
-        author:'Farid Suryanto',
-        publisher:'Sunhouse Digital',
-        year:2020,
-        isbn:'8298377474',
-        price:70000
-      },
-      {
-        title:'Membuat Aplikasi Maps menggunakan Angular',
-        author:'Farid Suryanto',
-        publisher:'Sunhouse Digital',
-        year:2020,
-        isbn:'82983323455',
-        price:75000
-      }
-    ];
-  }  
-
-  productDetail(data: any,idx: number)
-  {
-    let dialog=this.dialog.open(ProductDetailComponent, {
-      width:'400px',
-      data:data
-    });
-    dialog.afterClosed().subscribe(res=>{
-      if(res)
-      {
-         //jika idx=-1 (penambahan data baru) maka tambahkan data
-        if(idx==-1)this.books.push(res);      
-         //jika tidak maka perbarui data  
-        else this.books[idx]=res; 
-      }
-    })
-  }
- 
-  deleteProduct(idx: any)
-  {
-    var conf=confirm('Delete item?');
-    if(conf)
-    this.books.splice(idx,1);
-  }
- 
-
 }
